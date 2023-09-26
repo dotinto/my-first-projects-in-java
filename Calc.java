b55f75bfd14aaa880f6088e8de27922efb59e147
dotinto <corriolgamer@gmail.com>
2023-09-26 12:42:21 +0300

    added calc.java

---
+++ Calc.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите способ вычисления (+, -, *, /): ");
+        char s = scanner.next().charAt(0);
+        System.out.print("Введите первое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = scanner.nextInt();
+        scanner.close();
+        switch (s) {
+            case '+':
+                System.out.println("Результат: " + (a + b));
+                break;
+            case '-':
+                System.out.println("Результат: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Результат: " + (a * b));
+                break;
+            case '/':
+                System.out.println("Результат: " + (a / b));
+                break;
+            default:
+                break;
+        }
+    }
+}
